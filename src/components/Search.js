11598766b00af19c60b3443d10f0638eddc34c27
Jaxsonman <42976152+Jaxsonman@users.noreply.github.com>
2018-11-16 15:13:55 -0700

    front end connected

---
+++ src/components/Search.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+
+
+const  INITIAL_STATE = {
+    search: '',
+}
+
+
+export default class Search extends Component {
+    constructor(){
+        super()
+        this.state = {
+            search: '',
+        }
+        this.state = INITIAL_STATE
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleOnClick = this.handleOnClick.bind(this);
+
+    }
+
+    
+
+
+    handleChange(event) {
+        this.setState({ [event.target.name]: event.target.value})
+        
+    }
+
+    handleOnClick(e) {
+        e.preventDefault();
+        console.log('this was clicked')
+        // axios.defaults.headers.post['crossDomain'] = true;
+        // axios.post('http://localhost:5000/jobs', JSON.stringify(this.state))
+        // .then(res => {
+        //     console.log(res);
+        //     console.log(res.data);
+        // })
+        fetch('http://localhost:5000/jobs',{
+            method: 'POST',
+            mode: "no-cors",
+            body: JSON.stringify(this.state),
+            header: {
+                "Content-Type": 'application/json'
+            },
+        })
+        .then(response => console.log(response))
+
+        
+    }
+
+    render() {
+        return(
+            <form onSubmit={this.inputData}>
+                <input type="text" name="search" placeholder="Search Job Here" onChange={event => this.handleChange(event) } />
+                <button onClick={this.handleOnClick}>Search</button>
+                {console.log(this.state)}
+            </form>
+        )
+    }
+} 
--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import Search from './Search';
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+        <h1>Find Jobs Easier</h1>
+        <h2>Just Search Here</h2>
+        <Search />
       </div>
     );
   }
